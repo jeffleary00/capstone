e30e235fe643ea1c22b46ccf7c639004b674247e
J Leary <jeffleary00@gmail.com>
2020-09-12 10:45:10 -0400

    change to proper dist/js call in html

--- static/js/src/index.js
+++ static/js/src/index.js
@@ -6,6 +6,7 @@ let auth0 = null;
 let user = null;
 
 window.addEventListener('load', async () => {
+  console.log("creating auth0 client");
   auth0 = await createAuth0Client({
     domain: 'dev-us1d520w.us.auth0.com',
     client_id: '1XMqKvqO5JYM8GNh1oEl28voSfef8EKa',
@@ -13,24 +14,23 @@ window.addEventListener('load', async () => {
   });
 });
 window.addEventListener('load', async () => {
-  await auth0.handleRedirectCallback();
-});
-window.addEventListener('load', async () => {
-  user = await auth0.getUser();
-  console.log(user);
+  if (auth0) {
+    await auth0.handleRedirectCallback();
+    user = await auth0.getUser();
+    console.log(user);
+  }
 });
 
-if (auth0 === null) {
+if (auth0) {
+  m.render(root, [
+    m("div", "welcome registered user")
+  ]);
+} else {
   m.render(root, [
     m("div", "welcome unknown guest"),
     m("button.button", {id: "login"}, "login")
   ]);
-} else {
-  m.render(root, [
-    m("div", "welcome registered user")
-  ]);
 }
-
 
 document.getElementById('login').addEventListener('click', async () => {
   await auth0.loginWithRedirect();

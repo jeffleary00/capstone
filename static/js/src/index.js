1f096d1a3a99e13093f8ffd6ced803a6bec6f08a
J Leary <jeffleary00@gmail.com>
2020-09-12 10:36:14 -0400

    change to proper dist/js call in html

--- static/js/src/index.js
+++ static/js/src/index.js
@@ -11,16 +11,26 @@ window.addEventListener('load', async () => {
     client_id: '1XMqKvqO5JYM8GNh1oEl28voSfef8EKa',
     redirect_uri: 'https://cs-monitor.herokuapp.com'
   });
-
+});
+window.addEventListener('load', async () => {
   await auth0.handleRedirectCallback();
+});
+window.addEventListener('load', async () => {
   user = await auth0.getUser();
   console.log(user);
 });
 
-m.render(root, [
-  m("div", "welcome user"),
-  m("button.button", {id: "login"}, "login")]
-);
+if (auth0 === null) {
+  m.render(root, [
+    m("div", "welcome unknown guest"),
+    m("button.button", {id: "login"}, "login")
+  ]);
+} else {
+  m.render(root, [
+    m("div", "welcome registered user")
+  ]);
+}
+
 
 document.getElementById('login').addEventListener('click', async () => {
   await auth0.loginWithRedirect();
